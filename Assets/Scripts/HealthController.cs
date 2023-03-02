adbbbe5abebfc927a651fdf12f8101e8845b282a
ciliux <lugnius5a@gmail.com>
2023-03-02 21:29:18 +0200

    Fixes

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -6,41 +6,27 @@ public class HealthController : MonoBehaviour
 {
     public float health = 3f;
     public GameObject gameObject;
-    public float damageCooldown = 0.5f;
-    private float damageCooldownCounter = 0;
 
-    public void Damage(float damagePoints) {
-        if(damageCooldownCounter <= 0)
+    public void Damage(float damagePoints)
+    {
+        if (gameObject.GetComponent<PlayerMovement>() != null)
         {
-            if (gameObject.GetComponent<PlayerMovement>() != null)
-            {
-                if(gameObject.GetComponent<PlayerMovement>().isDashing != true)
-                {
-                    health -= damagePoints;
-                    FindObjectOfType<AudioManager>().Play("PlayerHit");
-                    damageCooldownCounter = damageCooldown;
-                }
-            }
-            else
+            if (gameObject.GetComponent<PlayerMovement>().isDashing != true)
             {
                 health -= damagePoints;
                 FindObjectOfType<AudioManager>().Play("PlayerHit");
-                damageCooldownCounter = damageCooldown;
             }
         }
-        //Debug.Log("Hit " + health);
-        if (health <= 0) {
+        else
+        {
+            health -= damagePoints;
+            FindObjectOfType<AudioManager>().Play("PlayerHit");
+        }
+        if (health <= 0)
+        {
             Destroy(this.gameObject);
             GameObject.FindWithTag("GameOverScreen").GetComponent<GameOverScreen>().Setup();
             FindObjectOfType<AudioManager>().Play("PlayerDeath");
         }
     }
-
-    private void Update()
-    {
-        if (damageCooldownCounter > 0)
-        {
-            damageCooldownCounter -= Time.deltaTime;
-        }
-    } 
 }
